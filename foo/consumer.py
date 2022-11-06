cb0074760a6fa9eeb7b52f06cf38cfb71b6fc2c1
DESKTOP-6VIARKL\Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-11-06 01:23:59 +0100

    - Consumer template

--- foo/consumer.py
+++ foo/consumer.py
@@ -1,4 +1,4 @@
-
+import requests
 
 class Game:
     def __init__(self, name):
@@ -7,3 +7,24 @@ class Game:
 
     def get_custom_name(self):
         return self.custom_name
+
+# Path from https://www.freetogame.com/api-doc
+api_url = "https://www.freetogame.com/api/games?platform=pc"
+
+# Get the response from the API
+response = requests.get(api_url)
+
+# Get the JSON data from the response
+data = response.json()
+
+# Create a list of Game objects
+games = []
+
+# Iterate over the data and create a Game object for each item
+for game in data:
+    games.append(Game(game["title"]))
+
+# Print the name of each game
+for game in games:
+    print(game.get_custom_name())
+
